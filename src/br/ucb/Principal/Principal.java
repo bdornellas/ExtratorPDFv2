809eb27f46d312e2ea5e59d616900eb74b2ac10f
bdornellas <bruno.b.dornellas@hotmail.com>
2018-12-26 14:11:59 -0200

    ExtratorPDF2

---
+++ src/br/ucb/Principal/Principal.java
@@ -0,0 +1,10 @@
+package br.ucb.Principal;
+import br.ucb.Janela.Janela;
+
+public class Principal {
+	public static void main(String[] args) {
+		//Instancia da janela		
+		Janela janela=new Janela();
+		janela.criajanela();
+	}
+}
